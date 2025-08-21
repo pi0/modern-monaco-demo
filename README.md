4eb12bf529e948b2a13884473f8dea39afea7fbe
Pooya Parsa <pooya@pi0.io>
2025-08-21 12:41:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Modern Monaco Demo
+
+A demo of [modern-monaco](https://github.com/esm-dev/modern-monaco) (by [@ije](https://github.com/ije)) with Server Side rendering.
+
+Demo displays its own server code inside the editor.
+
+- [View Source](./api/server.ts)
+- [Example Deployment](https://modern-monaco-demo.vercel.app/)
---
+++ api/server.ts
@@ -0,0 +1,36 @@
+import { renderToWebComponent } from "modern-monaco/ssr";
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from "node:url";
+
+// Demo displays its own server code inside the editor
+// Powered by https://github.com/esm-dev/modern-monaco
+// Demo source: https://github.com/pi0/modern-monaco-demo
+
+export default {
+  async fetch(req: Request): Promise<Response> {
+    const selfSrc = await readFile(fileURLToPath(import.meta.url), 'utf8')
+
+    const editor = await renderToWebComponent(
+      selfSrc,
+      {
+        language: "typescript",
+        userAgent: req.headers.get("user-agent"), // use system font
+      },
+    );
+    return new Response(
+      /* html */ `
+      <html>
+      <head>
+        <style> body { background-color: #121212 } </style>
+      </head>
+      <body>
+      ${editor}
+      <script type="module">
+        import { hydrate } from "https://esm.sh/modern-monaco";
+        hydrate(); // hydrate the editor
+      </script>
+    `,
+      { headers: { "Content-Type": "text/html" } },
+    );
+  },
+};
