7581ab6c99dcb619d0aa42f2b4a81ce7dbd1e52b
Je Xia <i@jex.me>
2025-08-22 00:38:00 +0800

    Use random theme (#3)

--- api/server.ts
+++ api/server.ts
@@ -4,27 +4,53 @@ import { renderToWebComponent } from "modern-monaco/ssr";
 // Powered by https://github.com/esm-dev/modern-monaco
 // Demo source: https://github.com/pi0/modern-monaco-demo
 
-const RAW_SOURCE =
-  "https://raw.githubusercontent.com/pi0/modern-monaco-demo/refs/heads/main/api/server.ts";
+const RAW_SOURCE = "https://raw.githubusercontent.com/pi0/modern-monaco-demo/refs/heads/main/api/server.ts";
 
-const THEME = "min-dark";
+const THEMES = [
+  "andromeeda",
+  "aurora-x",
+  "ayu-dark",
+  "catppuccin-mocha",
+  "dark-plus",
+  "dracula",
+  "everforest-dark",
+  "github-dark",
+  "gruvbox-dark-soft",
+  "material-theme-darker",
+  "min-dark",
+  "night-owl",
+  "one-dark-pro",
+  "rose-pine-moon",
+  "slack-dark",
+  "solarized-dark",
+  "tokyo-night",
+  "vesper",
+  "vitesse-dark",
+] as const;
 
 export default {
   async fetch(req: Request): Promise<Response> {
     const code = await fetch(RAW_SOURCE).then((r) => r.text());
-
+    const theme = THEMES[Math.floor(Math.random() * THEMES.length)];
     const userAgent = req.headers.get("user-agent");
 
     if (userAgent.startsWith("curl/")) {
       delete globalThis.process.env.NO_COLOR;
       globalThis.process.env.FORCE_COLOR = "2"; // 256 colors
       const { codeToANSI } = await import("@shikijs/cli");
-      return new Response(await codeToANSI(code, "typescript", THEME));
+      return new Response(await codeToANSI(code, "typescript", theme));
     }
 
     const editor = await renderToWebComponent(
       { code, filename: "server.ts" },
-      { theme: THEME, userAgent /* use system font */ }
+      {
+        theme,
+        padding: {
+          top: 10,
+          bottom: 10,
+        },
+        userAgent, /* use system font */
+      },
     );
 
     return new Response(
@@ -33,7 +59,7 @@ export default {
       <head>
         <title>Modern Monaco Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style> body { background-color: #121212 } </style>
+        <style> body { padding: 0; margin: 0 } </style>
       </head>
       <body>
       ${editor}
@@ -54,7 +80,7 @@ export default {
         });
       </script>
     `,
-      { headers: { "Content-Type": "text/html" } }
+      { headers: { "Content-Type": "text/html" } },
     );
   },
 };
